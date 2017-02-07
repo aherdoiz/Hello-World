c5e5615c52063b698a9d7b2353e67257a38c158d
aherdoiz <vendettapwnt@gmail.com>
2017-02-07 10:55:00 -0600

    Add files via upload

---
+++ test.js
@@ -0,0 +1,11 @@
+//Javascript source code
+var button = document.querySelector("button");
+
+var color = false;
+
+button.addEventListener("click", function() {
+    document.body.classList.toggle("color");
+})
+
+
+
